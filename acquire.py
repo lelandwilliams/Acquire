e720d811d0f4e08d80453edc73c18d834ebf45cf
Lee Williams <will4379@umn.edu>
2016-06-22 22:21:57 -0500

    fixes

--- acquire.py
+++ acquire.py
@@ -1,6 +1,4 @@
 import sys, string, random
-#from PyQt5.QtWidgets import QApplication
-#from mainWidget import AcquireUI
 
 class Player:
     def __init__(self, name, playerType):
@@ -10,14 +8,14 @@ class Player:
         self.money = 6000
 
     def __repr__(self):
-        s = "\t"
+        s = '\t'
         s.append(self.name)
         s.append(" $")
         s.append(self.money)
         s.append(" ")
         for i in self.hand:
             s.append(i)
-            s.append(" ")
+        s.append(" \n")
         return s
 
 class Corp:
@@ -30,19 +28,26 @@ class Corp:
 
     def __repr__(self):
         s = "\t"
-        s.append(self.name)
-        s.append(" active: ")
+        s += (self.name)
+        s += (" active: ")
         if self.active:
-            s.append("Yes  shares outstanding: ")
+            s += ("Yes  shares outstanding: ")
         else:
-            s.append("No   shares outstanding: ")
-        s.append(self.shares_available)
-        s.append(" price: $ ")
-        s.append(self.price())
+            s += ("No   shares outstanding: ")
+        s += str(self.shares_available)
+        s += (" price: $ ")
+        s += str(self.price())
+        s += ('\n')
         return s
 
     def isActive(self):
         return self.active
+
+    def setAnchorTile(self, tile):
+        self.anchor_tile = tile
+
+    def setActive(self, b):
+        self.active = b
 
     def setInitialPrice(self):
         if self.name in ["Worldwide", "American", "Festival"]:
@@ -58,17 +63,20 @@ class Corp:
 class Acquire:
     def __init__(self, players):
         self.corpNames = ["Tower","Luxor","Worldwide","Festival","American",
-                "Continental","Imperial"]:
+                "Continental","Imperial"]
         self.players = players
         self.currentPlayerNumber = 0
         self.tiles = self.initiate_tiles()
         self.tilegroups = []
-        for i in range(7):
-            self.tilegroups.append([])
-
         self.corporations = self.initiate_corps()
 
         random.shuffle(self.players)
+
+    def __repr__(self):
+        s = "Corporations\n"
+        for corp in self.corporations:
+            s += str(self.corporations[corp])
+        return s
 
     def addTiletoGroup(self,tile,group):
         index = self.tilegroups.index(group)
@@ -80,9 +88,22 @@ class Acquire:
             if self.isAdjoining(tile, self.tilegroups[i]):
                 adjoininggrouplist.append(i)
         return adjoininggrouplist
+
+    def adjoiningCorps(self, tile):
+        corps = []
+        groups = self.adjoiningGroups(tile)
+        for corp in self.corporations:
+            for group in groups:
+                if self.corporations[corp].anchorTile in self.tilegroups[group]:
+                    corps.append(corp)
+        return corps
                 
     def advanceCurrentPlayer(self):
         self.currentPlayerNumber = (self.currentPlayerNumber + 1 ) % len(self.players) 
+
+    def aiChooseCorp(self, corps):
+        random.shuffle(corps)
+        return corps[0]
 
     def aiChooseTile(self,player):
         random.shuffle(player.hand)
@@ -92,9 +113,9 @@ class Acquire:
         if current >= len(arr):
             return start
 
-        if arr[start][2] < arr[current][2]:
+        if arr[start][1] < arr[current][1]:
             return self.determineStartingPlayer(arr, start, current +1)
-        elif arr[start][2] > arr[current][2]:
+        elif arr[start][1] > arr[current][1]:
             return self.determineStartingPlayer(arr, current, current +1)
         else:
             if arr[start][0] < arr[current][0]:
@@ -108,6 +129,13 @@ class Acquire:
     def getCurrentPlayer(self):
         return self.players[self.currentPlayerNumber]
 
+    def inactiveCorps(self):
+        a = []
+        for corp in self.corporations:
+            if not self.corporations[corp].isActive():
+                a.append(corp)
+        return a
+
     def initiate_corps(self):
         a = {}
         for name in self.corpNames:
@@ -118,20 +146,35 @@ class Acquire:
         tiles = []
         for i in string.ascii_uppercase[:9]:
             for j in range(1,13):
-                tiles.append(str(j) + '-' + i)
+                tiles.append( (j,i) )
         random.shuffle(tiles)
         return tiles
 
     def isAdjoining(self,tile,group):
-        if (tile[0] + '-' + chr(ord(tile[2])+1))in group:
+        if (tile[0],str(ord(tile[1])+1)) in group:
             return True
-        if (tile[0] + '-' + chr(ord(tile[2])-1)) in group:
+        if (tile[0],str(ord(tile[1])-1)) in group:
             return True
-        if (chr(ord(tile[0])+1) + '-' + tile[2]) in group:
+        if (tile[0]+1,tile[1]) in group:
             return True
-        if (chr(ord(tile[0])-1) + '-' + tile[2]) in group:
+        if (tile[0]-1,tile[1]) in group:
             return True
         return False
+
+    def placeTile(self, tile, starters = True):
+        groups = self.adjoiningGroups(tile)
+        if len(groups) > 0:
+            groups[0].append(tile)
+            while len(groups) > 1:
+                self.tilegroups[groups[0]] += self.tilegroups[groups[1]] 
+                del self.tilegroups[self.tilegroups.index(groups[1])] 
+            self.tilegroups[groups[0]].append(tile)
+        else:
+            self.tilegroups.append([tile])
+
+    def setActive(self, corp, player, tile):
+        self.corporations[corp].setActive(True)
+        self.corporations[corp].setAnchorTile(tile)
 
     def setStarters(self):
         starters = []
@@ -139,19 +182,19 @@ class Acquire:
             starters.append(self.tiles.pop())
 
         for tile in starters:
-            groups = self.adjoiningGroups(tile)
-            if len(groups) > 0:
-                while len(groups) > 1:
-                    self.tilegroups[groups[0]] += self.tilegroups[groups[1]] 
-                    del self.tilegroups[groups[1]] 
-                    groups = self.adjoiningGroups(tile)
-                self.tilegroups[groups[0]].append(tile)
-            else:
-                self.tilegroups.append([tile])
+            self.placeTile(tile, True)
 
         self.currentPlayerNumber = self.determineStartingPlayer(starters)
-
         return starters
+
+    def tileToStr(self, tile):
+        return str(tile[0]) + "-" + tile[1]
+
+players = []
+players.append(Player('Bender', 'Robot'))
+players.append(Player('C3P0', 'Robot'))
+players.append(Player('Bender', 'Robot'))
+players.append(Player('Puny Human', 'Human'))
 
 def play():
     game = Game()
