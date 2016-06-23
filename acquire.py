36fc88fc5e61d8ecd5419ef144767a9f0b94ac27
Lee Williams <will4379@umn.edu>
2016-06-22 22:56:20 -0500

    added Acquire.evaluatePlay() and Acauire.corpSize()

--- acquire.py
+++ acquire.py
@@ -109,6 +109,12 @@ class Acquire:
         random.shuffle(player.hand)
         return player.hand.pop()
 
+    def corpSize(self, corp):
+        for group in self.tilegroups:
+            if self.corporations[corp].anchor_tile in group:
+                return len(group)
+        return 0
+
     def determineStartingPlayer(self, arr, start=0, current=1):
         if current >= len(arr):
             return start
@@ -122,6 +128,25 @@ class Acquire:
                 return self.determineStartingPlayer(arr, start, current +1)
             elif arr[start][0] > arr[current][0]:
                 return self.determineStartingPlayer(arr, current, current +1)
+
+    def evaluatePlay(self, tile):
+        if len(self.adjoiningGroups(tile)) == 0:
+            return "Regular"
+        if len(self.adjoiningCorps(tile)) == 0:
+            if self.activeCorps() == 7:
+                return "Illegal"
+            return "NewCorp"
+        if len(self.adjoiningCorps(tile)) == 1:
+            return "Regular"
+        safe_corps = False
+        for corp in self.adjoiningCorps(tile):
+            if self.corpSize(corp) > 10:
+                safe_corps += 1
+        if safe_corps > 1:
+            return "Illegal"
+        return "Regular"
+
+
 
     def fillHands(self):
         for player in self.players:
