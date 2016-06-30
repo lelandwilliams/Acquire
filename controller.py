0536bdd3633ebad7df1c8d9c5d4891a18043c1ad
Lee Williams <will4379@umn.edu>
2016-06-30 11:15:40 -0500

    Created controller.setup() and controller.chooseTile() and factored out appropriate code from newGame()

--- controller.py
+++ controller.py
@@ -4,7 +4,35 @@ class Controller:
     def __init__(self):
         pass
  
+    def chooseTile(self):
+        player = self.game.getCurrentPlayer()
+        if self.debug:
+            print(str(self.game))
+        tile = self.chooseTile(player)
+        outcome = self.game.evaluatePlay(tile)
+        if outcome == "Regular":
+            self.game.placeTile(tile)
+            self.changeTileColor(tile, 'None')
+        elif outcome == "NewCorp":
+            newcorp = self.chooseNewCorp(player,tile)
+            self.game.setActive(newcorp,player,tile)
+            self.changeGroupColor(newcorp)
+        elif outcome == "Addon":
+            self.game.placeTile(tile)
+            self.changeGroupColor(self.game.adjoiningCorps(tile)[0])
+        player.hand.remove(tile)
+        player.hand.append(self.game.tiles.pop())
+        player.hand.sort()
+
+
     def newGame(self):
+        self.setup()
+        while(not self.game.gameOver()):
+            self.chooseTile()
+            self.choseStock()
+            self.game.advanceCurrentPlayer()
+
+    def setup(self):
         players = self.setPlayers()
         self.game = acquire.Acquire(players)
         self.addPlayers(self.game.players)
@@ -13,26 +41,5 @@ class Controller:
         for tile in starters:
             self.changeTileColor(tile, 'None')
         self.game.fillHands()
-        while(not self.game.gameOver()):
-            player = self.game.getCurrentPlayer()
-            if self.debug:
-                print(str(self.game))
-            tile = self.chooseTile(player)
-            outcome = self.game.evaluatePlay(tile)
-            if outcome == "Regular":
-                self.game.placeTile(tile)
-                self.changeTileColor(tile, 'None')
-            elif outcome == "NewCorp":
-                newcorp = self.chooseNewCorp(player,tile)
-                self.game.setActive(newcorp,player,tile)
-                self.changeGroupColor(newcorp)
-            elif outcome == "Addon":
-                self.game.placeTile(tile)
-                self.changeGroupColor(self.game.adjoiningCorps(tile)[0])
-
-            player.hand.remove(tile)
-            player.hand.append(self.game.tiles.pop())
-            player.hand.sort()
-            self.game.advanceCurrentPlayer()
 
 
