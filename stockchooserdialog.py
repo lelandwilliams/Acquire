022e985da21d1d79de0397f03675ed1564943dc0
Lee Williams <will4379@umn.edu>
2016-07-05 11:45:03 +0000

    changed dialog code to reflect change of corps from a list to a dictionary

--- stockchooserdialog.py
+++ stockchooserdialog.py
@@ -5,19 +5,17 @@ class StockChooserDialog(QDialog):
     chosen = pyqtSignal(str)
     def __init__(self, companyList, colors):
         super().__init__()
-        self.company_list = companyList
-
         self.width = 80
 
         l = QHBoxLayout()
-        for company in companyList:
-            newStock = QPushButton(company + "\nHotels")
+        for corp, price in companyList.items():
+            newStock = QPushButton(corp + "\nHotels\n\n" + str(price))
             newStock.setStyleSheet("QPushButton {background-color: " + colors[company] + ";"
                         "color: white;"
                         "border-color: white;"
                         "border-style: outset;}")
             newStock.clicked.connect(self.buttonClicked)
-            newStock.setObjectName(company)
+            newStock.setObjectName(corp)
             newStock.setFixedSize(self.width, int(1.5 *self.width))
             l.addWidget(newStock)
         self.setLayout(l)
