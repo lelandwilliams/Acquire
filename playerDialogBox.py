82c464bbfba65aefc92d32426d151244b5b1d83f
Lee Williams <will4379@umn.edu>
2016-07-16 12:21:34 -0500

    Added explanatory text to dialogs

--- playerDialogBox.py
+++ playerDialogBox.py
@@ -15,29 +15,48 @@ class PlayerDialogBox(QFrame,QObject):
         self.setLayout(self.layout)
 
     def chooseMerger(self, corps, colors):
-        print("In Merger Dialog Box")
         self.dialog = StockChooserDialog(corps,colors)
-        self.label1.setText("You have caused a Merger. \n \
-        which Company should remain ?")
+        text = "<h1> Liquidate a Company</h1>"
+        text+= "<p>The tile you placed connected two or more corporations.</p>"
+        text += "<p>Usually the smallest corporation is absorbed into the larger<br>"
+        text += "but in this case there were two equal sized corporations</p>"
+        text += "<p>now you choose which of the corporations will be absorbed</p>"
+        self.label1.setText(text)
         self.layout.addWidget(self.dialog)
         corp = list(corps)[self.dialog.exec()]
         return corp
 
-    def chooseMergerStockAction(self,player,corp,largestCorp, actions, colors):
+    def chooseMergerStockAction(self,player,activePlayer, corp,largestCorp, actions, colors):
+        text = "<h1>Choose how to Liquidate Stock</h1>"
+        if player.name == activePlayer.name:
+            text += "<p>" + "You"
+        else:
+            text += "<p>" + activePlayer.name
+        text += " played a tile that caused " + corp + "to be merged into <br>"
+        text += largestCorp +".<p>"
+        text += "<p> Now you must choose what to do with your remaining stock in"
+        text += corp + "</p>"
+        text += "<p> Depending on share availibilty, you may:"
+        text += "<li> Trade in two shares of " + corp + "for one share of " + largestCorp
+        text += "<li> Sell a shares to the bank"
+        text += "<li> Hold on to your shares hoping for the re-establishment of the corporation"
         self.dialog = MergerActionDialog(actions, colors)
-        self.label1.setText(corp + "will be liquidated. \nChoose  what to do with your stock.")
+        self.label1.setText(text)
         self.layout.addWidget(self.dialog)
         return actions[self.dialog.exec()]
 
 
     def chooseStock(self,number,companies,colors):
+        text = "<h1>Purchase Stock</h1>"
+        text += "<p>At this point in your turn you can purchase up to<br>"
+        text += "three shares of stock in active companies</p>"
         self.dialog = StockChooserDialog(companies,colors)
         if number == 1:
-            self.label1.setText("Choose your first stock")
+            self.label1.setText(text +"Choose your <b>first</b> stock")
         elif number == 2:
-            self.label1.setText("Choose your second stock")
+            self.label1.setText(text +"Choose your <b>second</b> stock")
         elif number == 3:
-            self.label1.setText("Choose your third stock")
+            self.label1.setText(text +"Choose your <b>third</b> stock")
         self.layout.addWidget(self.dialog)
         stock =  list(companies)[self.dialog.exec()]
         print(stock)
@@ -48,7 +67,15 @@ class PlayerDialogBox(QFrame,QObject):
         label = "<h1><font color=\"blue\">" + player.name + "</font> Choose a Tile</h1>\n"
         label += "<p> It's now your turn.</p>" 
         label += "<p> Choose one of your tiles to place on the board.</p> "
-        label += "<p>Tiles that are not legal plays will be ignored</p>"
+        label += "<li> Tiles that played next to (a) solitary tile(s) <br>"
+        label += "will form a new corporation</li>"
+        label += "<li> Tiles that are adjacent to (colored) corporation tiles <br>"
+        label += "will increase the size of the corporation and (often) its value.</li>"
+        label += "<li> Tiles that connect two corporations will cause a merger. </li>"
+        label += "<p><small>(Tiles that connect two companies that are both size 11<br>"
+        label += "or that would found a new company when there are already 7 companies<br>"
+        label += "on the board are <b>illegal</b> plays and will be ignored"
+        label += "</small></p>"
         self.dialog = TileChooserDialog(tiles)
         self.label1.setText(label)
         self.label1.setAlignment(Qt.AlignHCenter)
@@ -59,7 +86,12 @@ class PlayerDialogBox(QFrame,QObject):
 
     def chooseCorporation(self, corps, colors):
         self.dialog = StockChooserDialog(corps,colors)
-        self.label1.setText("You have founded a corporation. Which one ?")
+        text = "<h1>Found a New Corporation</h1>"
+        text += "<p> The tile you placed joined with one or more tiles. </p>"
+        text += "<p> This begins a new corporation </p>"
+        text += "<p> You choose which corporation to form </p>"
+        text += "<p> As a bonus, you get one share of stock in the new corporation </p>"
+        self.label1.setText(text)
         self.layout.addWidget(self.dialog)
         corp =  list(corps)[self.dialog.exec()]
         print(corp)
