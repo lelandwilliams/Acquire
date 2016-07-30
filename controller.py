f519cb8a782269eb518e725759b05835599f2bcb
Lee Williams <will4379@umn.edu>
2016-07-30 17:56:48 +0000

    added handling to resolveMergerAction() for Sell All and Trade All

--- controller.py
+++ controller.py
@@ -146,6 +146,17 @@ class Controller:
             player.stock[corp] -= 1
             self.game.corporations[corp].shares_available += 1
             player.money += self.game.corporations[corp].price()
+        elif result == "Trade All:"
+            while stock[corp] >= 2 and stock[largestCorp. >= 1:
+                player.stock[corp] -= 2
+                player.stock[largestCorp] += 1
+                self.game.corporations[corp].shares_available +=2
+                self.game.corporations[largestCorp].shares_available -=1
+        elif result == "Sell":
+            while stock[corp] >= 1:
+                player.stock[corp] -= 1
+                self.game.corporations[corp].shares_available += 1
+                player.money += self.game.corporations[corp].price()
 
 
     def rewardPrimaries(self, corp):
