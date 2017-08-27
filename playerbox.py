d6d93f219a9b445efcc0d95cc68d3a2384b0af81
Lee Wwilliams <lelandhwilliams@gmail.com>
2017-08-27 14:13:35 -0500

    playerbox.py: playerbox now displays stock holdings

--- playerbox.py
+++ playerbox.py
@@ -1,40 +1,54 @@
 from PyQt5.QtWidgets import QGroupBox, QVBoxLayout, QHBoxLayout, QFrame, QLabel
 from acquire_model import Acquire
+from PyQt5.QtCore import Qt
 
 class PlayerBox(QFrame):
-    def __init__(self):
-        """ The nameFrameLayout holds the player name, and current cash holding
-            The lastPlayFrameLayout remembers the player's last move, and which stocks they hold
+    def __init__(self, colors):
+        """ The topFrame holds the player name, and current cash holding
+            The bottomFrame remembers the player's last move, and which stocks they hold
         """
         super().__init__()
 
-        self.layout = QVBoxLayout()
-        self.nameFrame = QFrame()
-        self.lastPlayFrame = QFrame()
-        self.nameFrameLayout = QHBoxLayout()
+        self.colorPallet = colors
+        self.stockWidth = 20
+        self.stockHeight = 30
 
-        self.lastPlayFrameLayout = QHBoxLayout()
+        self.topFrame = QFrame()
+        self.bottomFrame = QFrame()
+        self.stockFrame = QFrame()
+
+        self.mainlayout = QVBoxLayout()
+        self.setLayout(self.mainlayout)
+        self.toplayout = QHBoxLayout()
+        self.bottomlayout = QHBoxLayout()
         self.stockFrameLayout = QHBoxLayout()
 
         self.nameLabel = QLabel()
         self.moneyLabel = QLabel()
-        self.nameFrameLayout.addWidget(self.nameLabel)
-        self.nameFrameLayout.addStretch()
-        self.nameFrameLayout.addWidget(self.moneyLabel)
-        self.nameFrame.setLayout(self.nameFrameLayout)
+        self.toplayout.addWidget(self.nameLabel)
+        self.toplayout.addStretch()
+        self.toplayout.addWidget(self.moneyLabel)
+        self.topFrame.setLayout(self.toplayout)
+        self.mainlayout.addWidget(self.topFrame)
 
         self.lastPlayLabel = QLabel()
-        self.lastPlayFrameLayout.addWidget(self.lastPlayLabel)
+        self.bottomlayout.addWidget(self.lastPlayLabel)
         self.corporationLabel = dict()
-        self.lastPlayFrameLayout.addLayout(self.stockFrameLayout)
         for corp in Acquire.corpNames:
             self.corporationLabel[corp] = QLabel()
+            self.stockFrameLayout.addWidget(self.corporationLabel[corp])
             self.corporationLabel[corp].hide()
-            self.lastPlayFrameLayout.addWidget(self.corporationLabel[corp])
+            color = self.colorPallet[corp]
+            self.corporationLabel[corp].setStyleSheet("QLabel {background-color: " + color + "; color: white;}")
+            self.corporationLabel[corp].setFixedSize(self.stockWidth, self.stockHeight) 
+            self.corporationLabel[corp].setAlignment(Qt.AlignCenter)
 
-        self.setLayout(self.layout)
-        self.layout.addWidget(self.nameFrame)
-        self.layout.addWidget(self.lastPlayFrame)
+        self.stockFrame.setLayout(self.stockFrameLayout)
+        self.bottomlayout.addWidget(self.stockFrame)
+        self.bottomFrame.setLayout(self.bottomlayout)
+
+        self.mainlayout.addWidget(self.topFrame)
+        self.mainlayout.addWidget(self.bottomFrame)
 
         self.setLineWidth(1)
         self.setFrameStyle(QFrame.Panel)
@@ -47,6 +61,14 @@ class PlayerBox(QFrame):
 
     def setMoney(self, money):
         self.moneyLabel.setText( "$ " + str(money))
+
+    def setStock(self, stocks):
+        for corp,numshares in stocks.items():
+            self.corporationLabel[corp].setText(str(numshares))
+            if numshares ==0:
+                self.corporationLabel[corp].hide()
+            else:
+                self.corporationLabel[corp].show()
 
     def test(self):
         self.setName("Jimmy")
