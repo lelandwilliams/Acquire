dae224ea34944c71f39e1147e1f7592ea7218dd2
Lee Wwilliams <lelandhwilliams@gmail.com>
2017-09-08 10:41:23 -0500

    acquire: updated class creation to use robotFactory properly

--- acquire.py
+++ acquire.py
@@ -5,6 +5,7 @@ from robotFactory import robotFactory
 
 if __name__ == "__main__":
     master = uuid.uuid4().hex
+    factory = robotFactory()
     a = Controller(master)
     b = AcquireClient()
     b.set_id(master)
@@ -14,8 +15,8 @@ if __name__ == "__main__":
     b.send_message("ADDPLAYER;" + master + ";C3P0;" + player2) 
     player3 = uuid.uuid4().hex
     b.send_message("ADDPLAYER;" + master + ";Hal;" + player3) 
-    player1class = robotFactory(None, "Bender", player1)
-    player2class = robotFactory(None, "C3PO", player1)
-    player3class = robotFactory(None, "Hal", player1)
+    player1class = factory.getAI(None, "Bender", player1)
+    player2class = factory.getAI(None, "C3PO", player1)
+    player3class = factory.getAI(None, "Hal", player1)
     b.send_message("BEGIN;" + master) 
 
