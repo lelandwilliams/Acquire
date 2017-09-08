bf742349c4679b5c4dc2ccfb4c4960696a6d64f7
Lee Wwilliams <lelandhwilliams@gmail.com>
2017-09-07 19:01:49 -0500

    acquire: added instructions to begin a game

--- acquire.py
+++ acquire.py
@@ -7,4 +7,14 @@ if __name__ == "__main__":
     a = Controller(master)
     b = AcquireClient()
     b.set_id(master)
+    player1 = uuid.uuid4().hex
+    b.send_message("ADDPLAYER;" + master + ";Bender;" + player1) 
+    player2 = uuid.uuid4().hex
+    b.send_message("ADDPLAYER;" + master + ";C3P0;" + player2) 
+    player3 = uuid.uuid4().hex
+    b.send_message("ADDPLAYER;" + master + ";Hal;" + player3) 
+    player1class = robotFactory(None, "Bender", player1)
+    player2class = robotFactory(None, "C3PO", player1)
+    player3class = robotFactory(None, "Hal", player1)
+    b.send_message("BEGIN;" + master) 
 
