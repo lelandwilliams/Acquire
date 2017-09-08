db65b905314fa72e78e437ab8c6427223c948b39
Lee Wwilliams <lelandhwilliams@gmail.com>
2017-09-08 09:52:57 -0500

    robotFactory: initial add

---
+++ robotFactory.py
@@ -0,0 +1,13 @@
+##################################################
+#          r o b o t F a c t o r y
+#
+# This class helps manage the various AI agents
+# classes and distributes them to requesters
+#
+##################################################
+include randomAI
+
+class robotFactory:
+    def __init__(self, robotType, name, acquire_id):
+        return randomAI(name, acquire_id)
+
