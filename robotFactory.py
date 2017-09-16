dff978fd0335bf873322cf453550313af05be586
Lee Wwilliams <lelandhwilliams@gmail.com>
2017-09-16 15:35:59 -0500

    robotFactory: startAI method spawns the client as a new subprocessw

--- robotFactory.py
+++ robotFactory.py
@@ -6,8 +6,21 @@
 #
 ##################################################
 from randomAI import randomAI
+import subprocess 
 
 class robotFactory:
     def getAI(self, robotType, name, acquire_id):
         return randomAI(name, acquire_id)
+
+    def startAI(self, robotType='Random', name = None, acquire_id = None):
+        process_list = ["python"]
+        if robotType == 'Random':
+            arg_list.append("randomAI.py")
+        if not name == None:
+            arg_list.append("-n")
+            arg_list.append(name)
+        if not acquire_id == None:
+            arg_list.append("-i")
+            arg_list.append(acquire_id)
+        subprocess.Popen(arg_list)
 
