c5feae4cb33a5a11958919ba87d01c205498ff18
Lee Wwilliams <lelandhwilliams@gmail.com>
2017-09-17 11:37:42 -0500

    robotNames: initial commit

---
+++ robotNames.py
@@ -0,0 +1,11 @@
+
+names = ["Coppelia", "Sulla", "Radius", "Damon", "Helena", "Talos",
+        "Olimpia", "Hadaly", "Tik-Tok", "Zat", "Robbie", "Speedy",
+        "Daneel", "Gisgard", "Norby", "Astro Boy", "Bors", "SHROUD",
+        "Frost", "Pris", "Stepford", "Marvin", "Manders", "Mr. Roboto",
+        "Tin Man", "Gort", "Robby", "C-3P0", "4-LOM", "TC-14", "C1-10P",
+        "KH-47", "Optimus Prime", "Data", "Chip", "Ash", "Mother",
+        "Galaxina", "Aqua", "T-800", "Bishop", "J5", "Bender", "K-2SO",
+        "Rosie", "Gigantor", "Hymie", "Joe 90", "K9", "S.A.M", "7-Zark-7",
+        "Twiki", "Mecha-Streisand", "HAL-9000", "Unimate", "Asimo", "Shakey",
+        "Zhora"] 
