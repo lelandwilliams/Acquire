e3281d6c55e8e0a8e89edd92b55d60d618c3a78c
Lee Wwilliams <lelandhwilliams@gmail.com>
2017-09-19 19:07:59 -0500

    acquire: removed obsolete includes

--- acquire.py
+++ acquire.py
@@ -1,12 +1,10 @@
-import sys, uuid, subprocess
+import subprocess
 from controller import Controller
-#from network import AcquireClient, AcquireSimpleLogger
 from robotFactory import robotFactory
 from PyQt5.QtCore import QCoreApplication, pyqtSlot, QTimer
 
 if __name__ == "__main__":
-#   print("starting server")
-#   print("starting client")
+    print("Starting")
     subprocess.Popen(["python", "serverTest.py"])
     subprocess.Popen(["python", "clientTest.py"])
     factory = robotFactory()
