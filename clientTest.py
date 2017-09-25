21a073d644eca321d2028a6dd3a223a71335ff30
Lee Wwilliams <lelandhwilliams@gmail.com>
2017-09-25 09:40:20 -0500

    clientTest: removed some old test code

--- clientTest.py
+++ clientTest.py
@@ -5,5 +5,4 @@ from PyQt5.QtCore import QCoreApplication
 if __name__ == "__main__":
     app = QCoreApplication(sys.argv)
     a = AcquireSimpleLogger()
-#   a.outgoing_message_q.put("HiHiHiHi")
     sys.exit(app.exec_())
