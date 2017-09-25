0ee365db0e2c7ec2ef665dff7954c38fc36df7c7
Lee Wwilliams <lelandhwilliams@gmail.com>
2017-09-25 09:41:10 -0500

    randomAI: created overides of process_x functions

--- randomAI.py
+++ randomAI.py
@@ -6,10 +6,23 @@ class randomAI(AcquireClient):
     def __init__(self, name, acquire_id = None):
         self.name = name
         super().__init__()
-        self.outgoing_message_q.put("REGISTER;"+self.name+';')
+        self.outgoing_message_q.put("REGISTER;"+self.name)
+#       self.outgoing_message_q.put("KILL;"+self.name)
 
     def process_private(self, player, parameter):
         print(name +": received a private message")
+
+    def process_register(self, player):
+        print(name +": received a registration message")
+        QTimer.singleShot(250, self.main)
+
+    def process_disconnect(self, player, parameter):
+        print(name +": received a disconnect message")
+        QCoreApplication.quit()
+
+    def process_uuid(self, parameter):
+        print(name +": received a UUID message")
+        self.acquire_id = parameter
 
 if __name__ == "__main__":
     app = QCoreApplication(sys.argv)
