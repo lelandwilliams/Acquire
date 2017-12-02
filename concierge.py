96ba68588aac2d4a0531a1b909131ecda5711cc9
Lee Wwilliams <lelandhwilliams@gmail.com>
2017-12-02 14:50:59 -0600

    added in result storage

--- concierge.py
+++ concierge.py
@@ -1,6 +1,7 @@
-import sys, queue, uuid, argparse, subprocess
+import sys, queue, uuid, argparse, subprocess, statistics
 from PyQt5 import QtNetwork, QtWebSockets
 from PyQt5.QtCore import QObject, QDataStream, pyqtSlot, QCoreApplication, QTimer
+import model
 
 DEFAULTPORT = 64337
 
@@ -15,22 +16,39 @@ class Concierge(QObject):
         self.address = address
         self.readyServers = list()
         self.max_servers = 1
-        self.serverObjects = []
+        self.serverPort = 0
+        self.serverPorts = dict()
+        self.servers = list() # A place to store obj references so the garbarge collector
+                            # wont' take them away
 
         attempts = 0
         max_attempts = 10
+
         while attempts < max_attempts and not self.server.isListening():
             if self.server.listen(self.address, self.port):
                 self.server.newConnection.connect(self.newClient)
+#               print("Concierge listening on port {}".format(self.port))
+            else:
+                self.port += 1
+        if not self.server.isListening():
+            raise('Concierge unable to bind port')
+
+        self.game_seed = 0
+        self.num_games = 0
+        self.scores = list()
+        self.highscores = list()
+        self.of = open('results.txt', 'w')
+        self.of.write("{:^3}, {:^8}, {:^8}, {:^5}, {:^8}, {:^8}\n".format('num','avg','std','max','w_avg','w_std'))
+        self.of.close()
 
         self.runGames()
 
     def newClient(self):
-#       print('a new server connected to concierge')
+        print('a new server connected to concierge')
         client = self.server.nextPendingConnection()
         client.textMessageReceived.connect(self.processTextMessage)
         client.disconnected.connect(self.socketDisconnected)
-        self.serverObjects.append(client)
+        self.servers.append(client) # solely to keep the garbage collector from destroying
 
     def processTextMessage(self,message):
 #       print("Concierge recieved message: {}".format(message))
@@ -38,28 +56,66 @@ class Concierge(QObject):
         if len(message.split(';')) != 2:
                 return
         m_type,m_body = message.split(';')
-        print("Concierge recieved message: {}".format(m_type))
+#       print("Concierge recieved message: {}".format(m_type))
 
         if m_type == 'READY':
+            self.serverPorts[sender] = m_body
             self.serverReady(m_body)
         elif m_type == 'DONE':
-            self.serverDone(m_body)
+            self.serverDone(m_body, sender)
 
     def runGames(self):
+        self.num_games += 1
         if len(self.readyServers) < self.max_servers:
             subprocess.Popen(["python", "gameServer.py", "-cp", str(self.port), "-n", '4'])
 
-    def serverDone(self, port):
-        print("Server Done ")
+    def serverDone(self, game, server):
+#       print("Server Done ")
+        s, hist = eval(game)
+#       for h in hist:
+#           model.print_turn(h)
+#       for player in s['Players']:
+#           print("{}: {}".format(player, model.netWorth(player, s)))
+        scores = [model.netWorth(player, s) for player in s['Players'] if player != "Bank"]
+        self.highscores.append(max(scores))
+        self.scores += scores
+        if self.num_games < 10:
+            self.num_games += 1
+            print("\rRunning game # {}".format(self.num_games))
+            server.sendTextMessage("RESET")
+#           self.serverReady(self.serverPort)
+        else:
+            self.of = open('results.txt', 'a')
+            self.of.write("{:3d}, {:6.2f}, {:6.2f}, {:5}, {:6.2f}, {:6.2f}\n".format(self.game_seed,
+                    statistics.mean(self.scores),
+                    statistics.stdev(self.scores),
+                    max(self.highscores),
+                    statistics.mean(self.highscores),
+                    statistics.stdev(self.highscores)))
+            self.of.close()
+            server.sendTextMessage("DISCONNECT")
+            if self.game_seed < 1:
+                self.num_games = 0
+                self.scores = list()
+                self.highscores = list()
+                self.game_seed += 1
+                self.runGames()
+            else:
+                QCoreApplication.quit()
+
+#          print("Average Score was {:5.2f} with a std dev of {:5.2f}".format(
+#               statistics.mean(self.scores), statistics.stdev(self.scores)))
+#           print("Average High Score was {:5.2f} with a std dev of {:5.2f}".format(
+#               statistics.mean(self.highscores), statistics.stdev(self.highscores)))
 
     def serverReady(self, port):
-#       print("Concierge: a server said it is ready")
+        print("Concierge: a server said it is ready")
         process_list = list()
-        process_list.append(["python", "gameClient.py", "-p", port, "-n", "Max"])
-        process_list.append(["python", "gameClient.py", "-p", port, "-n", "Min1"])
-        process_list.append(["python", "gameClient.py", "-p", port, "-n", "Min2"])
-        process_list.append(["python", "gameClient.py", "-p", port, "-n", "Min3"])
-        process_list.append(["python", "GM.py", "-p", port])
+        process_list.append(["python", "randomClient.py", "-p", port, "-n", "Max"])
+        process_list.append(["python", "randomClient.py", "-p", port, "-n", "Min1"])
+        process_list.append(["python", "randomClient.py", "-p", port, "-n", "Min2"])
+        process_list.append(["python", "randomClient.py", "-p", port, "-n", "Min3"])
+        process_list.append(["python", "GM.py", "-p", port, "-s", str(self.game_seed)])
 
         for args in process_list:
             subprocess.Popen(args)
