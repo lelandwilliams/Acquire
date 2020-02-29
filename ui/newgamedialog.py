98956ff4e2d92d1ea43d1e6b39e8edb28154f818
Leland Williams <lelandhwilliams@gmail.com>
2020-02-29 16:54:28 -0600

    Created makeExamples() and linked the start button to it

--- ui/newgamedialog.py
+++ ui/newgamedialog.py
@@ -2,15 +2,18 @@ import sys
 from PyQt5.QtWidgets import QApplication, QFrame, QHBoxLayout, QVBoxLayout,\
 QLineEdit, QButtonGroup, QGridLayout, QRadioButton, QFileDialog, QPushButton,\
 QToolButton, QLabel
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QIntValidator
 import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 from robotNames import names as robotnames
 from random import choice
+from exampleMaker import statsBuilder, Bots
 
-playerTypes = ['Human', 'RandomAI']
+playerTypes = ['Human']
+for b in Bots:
+    playerTypes.append(b[:-3])
 
 class NewGameDialog(QFrame):
     """ Provides the contents of the dialog box.
@@ -44,6 +47,8 @@ class NewGameDialog(QFrame):
             self.saLabel2 = QLabel("Num Games:")
             self.saEditBar = QLineEdit(self.logfile)
             self.numBox = QLineEdit("10")
+            self.validator = QIntValidator(1,100)
+            self.numBox.setValidator(self.validator)
 
             self.standalonelayout.addWidget(self.saLabel)
             self.standalonelayout.addWidget(self.saEditBar)
@@ -57,6 +62,7 @@ class NewGameDialog(QFrame):
             self.buttonRow.addWidget(self.startButton)
             self.buttonRow.addWidget(self.quitButton)
             self.quitButton.clicked.connect(self.close)
+            self.startButton.clicked.connect(self.makeExamples)
 
             self.leftLayout.addLayout(self.standalonelayout)
             self.leftLayout.addLayout(self.buttonRow)
@@ -68,7 +74,7 @@ class NewGameDialog(QFrame):
         cur_names = [p.nameBox.text() for p in self.players]
         while name in cur_names or name == '':
             name = choice(robotnames)
-        player = PlayerBox(name, player_type)
+        player = PlayerBox(name, player_type, self.standalone)
         self.playerLayout.addWidget(player)
         self.players.append(player)
 
@@ -77,16 +83,27 @@ class NewGameDialog(QFrame):
         print(self.filename)
         self.saEditBar.setText(self.filename[0])
 
+    def makeExamples(self):
+        game_runner = statsBuilder()
+        player_dict = dict()
+        for p in self.players:
+            p_name = p.nameBox.text()
+            selected = p.typeGroup.checkedButton().text()
+            player_dict[p_name] = selected + ".py"
+        game_runner.players = player_dict
+
+
 
 class PlayerBox(QFrame):
     """ Proveds a Horizontal widget to specify player information when starting a game.
     Parameters:
     -----------
         name(str): the specified name for the player.
-        restricted_type(str): can be 'human' or 'robot' or ''.
+        player_type(str): the default player type for this player
+        standalong(bool): if the dialog is standalone, then all the clients should be bots
     """
     spacer_width = 0
-    def __init__(self, name = "", player_type = 'human'):
+    def __init__(self, name = "", player_type = 'human', standalone = False):
         super().__init__()
 
         self.layout = QHBoxLayout()
@@ -97,8 +114,14 @@ class PlayerBox(QFrame):
         self.nameBox.setReadOnly(True)
 
         # Add player type choices as radio buttons
+        playerOptions = []
+        if standalone:
+            playerOptions = playerTypes[1:]
+        else:
+            playerOptions = playerTypes
+
         i = 0
-        for pt in playerTypes:
+        for pt in playerOptions:
             button = QRadioButton(pt)
             self.button_layout.addWidget(button, 0, i)
             self.typeGroup.addButton(button, id = i)
