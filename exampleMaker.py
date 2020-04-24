9ee70a5050c531fba6ad0983ef1a5009e58aa63b
Leland Williams <lelandhwilliams@gmail.com>
2020-04-24 15:19:33 -0500

    added a mutex around num_games, and added signals finished and ompletedGames

--- exampleMaker.py
+++ exampleMaker.py
@@ -3,7 +3,7 @@
 from concierge import Concierge
 import model, train
 import subprocess, sys
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, QMutex, pyqtSignal
 from PyQt5.QtNetwork import QHostAddress
 
 DEFAULTPORT = 64337
@@ -24,10 +24,13 @@ class statsBuilder(Concierge):
 
 
     """
+    finished = pyqtSignal()
+    completedGames = pyqtSignal(int)
+
     def __init__(
             self,
-            my_id = None, 
-            port = DEFAULTPORT, 
+            my_id = None,
+            port = DEFAULTPORT,
             address = QHostAddress.LocalHost,
             num_servers = 1,
             players = None):
@@ -35,24 +38,33 @@ class statsBuilder(Concierge):
         self.players = players
         self.num_games = 0
         self.max_games = 1
-        self.fname = 'examples.gam'
+        self.fname = 'flex4_examples.gam'
+        self.mutex = QMutex()
+
 
     def serverDone(self, game, server):
-        try:
-            _ = train.reconstruct_states(game) 
-        except:
-            self.server.disconnect()
-            QCoreApplication.quit()
+#       try:
+#           _ = train.reconstruct_states(game)
+#       except:
+#           self.server.disconnect()
+#           QCoreApplication.quit()
+        self.mutex.lock()
         of = open(self.fname, 'a')
         of.write(game)
         of.write('\n')
         of.close()
+        self.num_games += 1
+        self.completedGames.emit(self.num_games)
         if self.num_games < self.max_games:
             server.sendTextMessage("RESET")
         else:
             server.sendTextMessage("DISCONNECT")
             self.servers_active -= 1
             self.server.disconnect()
+            self.finshed.emit()
+        self.mutex.unlock()
+        
+        if self.servers_active == 0:
             QCoreApplication.quit()
 
     def serverReady(self, port):
